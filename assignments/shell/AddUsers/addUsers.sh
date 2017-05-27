e90a776cf05b6ccc4bee6045e9f987eb5e4c472b
ShuYingShu <shuying97@163.com>
2017-05-27 17:25:35 +0800

    修改了addUser.sh,添加了delete.sh用来删除用户

--- assignments/shell/AddUsers/addUsers.sh
+++ assignments/shell/AddUsers/addUsers.sh
@@ -1,9 +1,9 @@
-#
-#
-# useradd
-# chpasswd
-# chown
-# chmod
-# edquota
-#
+#!/bin/bash
+# Linux101/assignments/shell/AddUsers/users.txt'
+for user in $(cat /root/Linux101/assignments/shell/AddUsers/users.txt)
+do
+	useradd $user
+	echo $user:ubuntu | chpasswd
+	echo "$user create success!"
+done
 
